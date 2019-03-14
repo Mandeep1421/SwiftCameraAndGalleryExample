62b130c2956f5d456f93d91b492f0ca14799b3b3
MacStudent <macstudent@126-imac16.local>
2019-03-13 21:24:00 -0400

    updating the code

--- SwiftCameraAndGalleryExample/ViewController.swift
+++ SwiftCameraAndGalleryExample/ViewController.swift
@@ -7,8 +7,8 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
+import MobileCoreServices
+class ViewController: UIViewController
 {
 
     @IBOutlet weak var imgPhoto: UIImageView!
@@ -25,9 +25,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             let myPickerController = UIImagePickerController()
             myPickerController.delegate = self;
             myPickerController.sourceType = .photoLibrary
+            myPickerController.mediaTypes = [kUTTypeImage as String]
+            myPickerController.allowsEditing = false
             self.present(myPickerController, animated: true, completion: nil)
+            newMedia = false
         }
-        
     }
     
     @IBAction func btnCamera(_ sender: UIBarButtonItem)
@@ -37,25 +39,57 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             let myPickerController = UIImagePickerController()
             myPickerController.delegate = self;
             myPickerController.sourceType = .camera
-           self.present(myPickerController, animated: true, completion: nil)
+            myPickerController.mediaTypes = [kUTTypeImage as String]
+            myPickerController.allowsEditing = false
+            self.present(myPickerController, animated: true, completion: nil)
+            newMedia = true
         }
-    }
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController)
-    {
-        self.dismiss(animated: true, completion: nil)
-    }
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
-    {
-        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
-        {
-            self.imgPhoto.image = image
-        }else
-        {
-            print("Something went wrong")
-        }
-        
-        self.dismiss(animated: true, completion: nil)
     }
     
+}
+    extension ViewController:  UIImagePickerControllerDelegate, UINavigationControllerDelegate
+    {
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+            let mediaType = info[UIImagePickerController.InfoKey.mediaType] as! NSString
+            
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                self.imgPhoto.image = image
+                //Save Image if taken from Camera
+                if (newMedia == true) {
+                    UIImageWriteToSavedPhotosAlbum(image, self, #selector(self.image(image:didFinishSavingWithError:contextInfo:)), nil)
+                } else if mediaType.isEqual(to: kUTTypeMovie as String) {
+                    // Code to support video here
+                }
+            }else{
+                print("Something went wrong")
+            }
+            
+            //Display all keys
+            for (k, _) in info {
+                print("TEST - \(k)")
+            }
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        @objc func image(image: UIImage, didFinishSavingWithError error: NSErrorPointer, contextInfo:UnsafeRawPointer) {
+            
+            if error != nil {
+                let alert = UIAlertController(title: "Save Failed",
+                                              message: "Failed to save image",
+                                              preferredStyle: UIAlertController.Style.alert)
+                
+                let cancelAction = UIAlertAction(title: "OK",
+                                                 style: .cancel, handler: nil)
+                
+                alert.addAction(cancelAction)
+                self.present(alert, animated: true,
+                             completion: nil)
+            }
+        }
 }
 
